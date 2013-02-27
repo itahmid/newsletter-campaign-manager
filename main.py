18cc7704d7b70d7447bef5b9291bfc5d8aff8dc1
erm <erm@datalore.us>
2013-02-27 15:55:34 -0600

    refactoring sql queries/campaign views for new database structures, fixed style of main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import os, sys
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, _app_ctx_stack, send_from_directory
+import os
+from flask import Flask, request, session, redirect, url_for, \
+    render_template, send_from_directory
 from flaskext.mysql import MySQL
 import settings
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'views')
 app = Flask(__name__, template_folder=tmpl_dir)
-app.config.update(DEBUG = True,)
+app.config.update(DEBUG=True,)
 
 app.secret_key = settings.secret_key
 mysql = MySQL()
+
 app.config.setdefault('MYSQL_DATABASE_PORT', 3306)
 app.config.setdefault('MYSQL_DATABASE_USER', settings.USER)
 app.config.setdefault('MYSQL_DATABASE_PASSWORD', settings.PASSWORD)
@@ -17,14 +18,17 @@ app.config.setdefault('MYSQL_DATABASE_DB', settings.DB)
 app.config.setdefault('MYSQL_DATABASE_CHARSET', 'utf8')
 mysql.init_app(app)
 
+
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 
-                                                'ico/favicon.ico')
+    return send_from_directory(os.path.join(
+        app.root_path, 'static'), 'ico/favicon.ico')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.route('/', defaults={'page': 1})
 @app.route('/page/<int:page>')
@@ -34,22 +38,24 @@ def index(page):
 
     conn = mysql.get_db()
     db = conn.cursor()
-    db.execute('SELECT COUNT(id) FROM newsletter')
-    count = db.fetchall()
-    db.execute("""SELECT * from `newsletter` 
+    # db.execute('SELECT COUNT(newsletters_id) FROM newsletters')
+    # count = db.fetchall()
+    db.execute("""SELECT * FROM `newsletters`
                 ORDER BY date_added DESC LIMIT 15 OFFSET %s""" % page)
     cols = tuple([d[0].decode('utf8') for d in db.description])
     _newsletters = [dict(zip(cols, row)) for row in db]
     newsletters = []
     for newsletter in _newsletters:
-        db.execute("""SELECT companies_title 
-                    FROM companies 
-                    WHERE companies_id = %d""" 
-                    % newsletter['companies_id'])
-        newsletter['companies_id'] = db.fetchall()[0][0]
+        if newsletter['company'] == 0:
+            newsletter['company'] = 'N/A'
+        else:
+            db.execute("""SELECT name FROM `companies`
+                        WHERE companies_id = %d""" % newsletter['company'])
+            newsletter['company'] = db.fetchall()[0][0]
         newsletters.append(newsletter)
 
-    return render_template('index.html', newsletters=newsletters, page= page)
+    return render_template('index.html', newsletters=newsletters, page=page)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -59,8 +65,8 @@ def login():
         username = request.form['username']
         password = request.form['password']
         db = mysql.get_db().cursor()
-        db.execute("""SELECT * FROM user WHERE user_name = '%s' 
-                    AND user_password='%s'""" % (username, password))
+        db.execute("""SELECT * FROM users WHERE name = '%s'
+                    AND password='%s'""" % (username, password))
         check = db.fetchall()
         if not check:
             error = 'Invalid username or password'
@@ -69,6 +75,7 @@ def login():
             return redirect(url_for('index'))
 
     return render_template('manage/login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
@@ -79,4 +86,4 @@ if __name__ == '__main__':
     from controllers import campaigns, subscribers
     app.register_blueprint(campaigns.mod)
     app.register_blueprint(subscribers.mod)
-    app.run(host='0.0.0.0', port = 5000, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
