d0d378ce931c602ef4a4121cac370ba493a3589b
erm <erm@datalore.us>
2013-03-13 14:24:10 -0500

    added lists to existing list page

--- pipmail/controllers/subscribers.py
+++ pipmail/controllers/subscribers.py
@@ -1,27 +1,40 @@
-from flask import Blueprint, request, abort, render_template
+from flask import Blueprint, request, abort, render_template, redirect, url_for
 from flaskext.mysql import MySQL
 from pipmail.auth import login_required
+import time
+import datetime
+
+error_dict = {'name': 'Please enter a name for this list',
+              'description': 'Please enter a brief description for this list',
+              'first_name': 'Please enter a first name for this recipient',
+              'last_name': 'Please enter a last name for this recipient',
+              'email': 'Please enter an email address for this recipient',
+              }
 
 
 mysql = MySQL()
 mod = Blueprint('subscribers', __name__)
 
 
-@mod.route('/lists', defaults={'page': 1})
+@mod.route('/lists', defaults={'page': 0})
 @mod.route('/lists/page/<int:page>')
 @login_required
 def lists(page):
     conn = mysql.get_db()
     db = conn.cursor()
-    #db.execute('SELECT COUNT(id) FROM newsletter')
-    #count = db.fetchall()
-    db.execute("""SELECT * from `lists`
-                ORDER BY date_created DESC LIMIT 15 OFFSET %s""" % page)
+    db.execute('SELECT COUNT(lists_id) FROM lists')
+    count = db.fetchall()
+    print count
+    db.execute("""SELECT * FROM `lists`
+                ORDER BY date_added DESC LIMIT 15 OFFSET %s""" % page)
     cols = tuple([d[0].decode('utf8') for d in db.description])
-    lists = [dict(zip(cols, row)) for row in db]
-    # print lists
-    # for l in lists:
-    #     print l
+    _lists = [dict(zip(cols, row)) for row in db]
+    lists = []
+    for lst in _lists:
+        unix_to_local = int(lst['date_added'])
+        lst['date_added'] = datetime.datetime.fromtimestamp(
+            unix_to_local).strftime('%Y-%m-%d %I:%M:%S')
+        lists.append(lst)
     return render_template('subscribers/lists.html', lists=lists, page=page)
 
 
@@ -29,9 +42,44 @@ def lists(page):
 @login_required
 def create_list():
     error = None
+    conn = mysql.get_db()
+    db = conn.cursor()
 
     if request.method == 'POST':
-        pass
+        errors = [opt for opt, val in request.form.iteritems()
+                  if (val == '')]
+        if len(errors) > 1:
+            error = [error_dict.get(err) for err in errors
+                     if error_dict.get(err) != '']
+        else:
+            for k, v in request.form.iteritems():
+                print k, v
+            try:
+                db.execute("""INSERT into lists
+                            (
+                                name,
+                                description,
+                                date_added
+                            )
+
+                            VALUES (
+                                %s, #name
+                                %s, #description
+                                %s) #date_added
+                            """,
+                           (
+                           request.form['name'],
+                           request.form['description'],
+                           int(time.time())
+                           )
+                           )
+                conn.commit()
+            except Exception, e:
+                print e
+                conn.rollback()
+            #return render_template('subscribers/lists.html', success=True,
+                                   #lists=lists, page=1)
+            return redirect(url_for('subscribers.lists'))
     return render_template('subscribers/details.html', error=error,
                            editing=False)
 
