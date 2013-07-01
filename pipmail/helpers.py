9c6f5cf368566472dd5ba88c8a2b8cc7e10ccaef
erm <erm@datalore.us>
2013-07-01 10:35:40 -0500

    removed old campaign files

--- pipmail/helpers.py
+++ pipmail/helpers.py
@@ -19,7 +19,6 @@ def collect_form_items(form):
     return 'lol'
     
 
-
 def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -35,20 +34,3 @@ def unix_to_local(timestamp):
     _time = datetime.datetime.fromtimestamp(
         ts).strftime('%Y-%m-%d %I:%M:%S')
     return _time
-
-
-
-
-
-
-
-#make a uniform search function for both controllers
-# def search():
-#     keywords = request.form['keywords']
-#     db = get_db()
-#     cur = db.execute("""SELECT paste_id, title, paste_text, date_str
-# FROM pastes WHERE paste_text LIKE '%?%'""",
-#                         (keywords,))
-#     result = fetch_result(cur.fetchall())
-
-#     return render_template("search.html", results=result)
